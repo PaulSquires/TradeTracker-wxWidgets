d9185a427ad50b582cd8ce8581dc7a725d76d0a5
Paul Squires <planetsquires@gmail.com>
2024-03-11 19:01:44 -0230

    Update build files

--- cmake_build.bat
+++ cmake_build.bat
@@ -1,8 +1,25 @@
-@echo off
+@ECHO off
 
-REM CONFIGURE
-cmake -S. -Bbuild
+REM *************************
+REM    CONFIGURE AND BUILD
+REM *************************
+CD %~dp0
+SET build_dir=build
+cmake -S. -B%build_dir%
+cmake --build %build_dir% -j
+IF %ERRORLEVEL% NEQ 0 (GOTO BUILD_FAILED) 
 
-REM BUILD
-cmake --build build -j
 
+REM *************************
+REM     RUN APPLICATION
+REM *************************
+IF "%~1"=="" exit 0
+SET app_name=%~dp0%build_dir%\Debug\%1.exe
+ECHO Run application: %app_name%
+START %app_name%
+EXIT 0
+
+
+:BUILD_FAILED
+ECHO Build Failed!
+EXIT 1
